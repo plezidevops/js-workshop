637f405b42660df326bfe3529d1b55e6c53d3c7c
Pascal M. Laurent <pascalmlaurent@gmail.com>
2022-05-24 14:05:26 -0400

    countdown with one parameter n

---
+++ js-basics/exercises/count_down.js
@@ -0,0 +1,11 @@
+// countdown with one parameter (n)
+function countdown(n) {
+  if (n < 1) {
+    return []
+  } else {
+    const countArray = countdown(n - 1)
+    countArray.unshift(n)
+    return countArray
+  }
+}
+console.log(countdown(5))
